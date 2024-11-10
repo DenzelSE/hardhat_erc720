a92829d65dbd0ff1fcb5895043713d913babf08a
DenzelSE <t.d.selokela@gmail.com>
2024-11-10 21:01:58 +0200

    mint function

--- contracts/ContractERC721.sol
+++ contracts/ContractERC721.sol
@@ -5,7 +5,17 @@ pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ContractERC721 is ERC721 {
-    constructor( string memory name, string memory symbol) ERC721(name, symbol){
-        
+    uint256 public tokenCounter;
+    
+    constructor() 
+    ERC721("protocols", "PTC"){
+        tokenCounter = 0;
+
     }
+
+    function _Mint(address recipient) public {
+        _safeMint(recipient, tokenCounter);
+        tokenCounter++;
+    }
+
 }
